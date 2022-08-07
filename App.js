0fef22c7efccaf7ce86069b2c3e9a75210ff5c9b
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2022-08-07 15:26:15 -0300

    feat: adding basket details and montserrat font

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar, StyleSheet, SafeAreaView } from "react-native";
+import {
+    useFonts,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
 import Basket from "./src/screens/Basket";
 
 export default function App() {
+    const [loaded] = useFonts({
+        Montserrat: Montserrat_400Regular,
+        MontserratBold: Montserrat_700Bold,
+    });
+
+    if (!loaded) return null;
+
     return (
         <SafeAreaView>
             <StatusBar />
--- src/screens/Basket.js
+++ src/screens/Basket.js
@@ -1,5 +1,6 @@
-import { Dimensions, Image, StyleSheet, Text } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import top from "../../assets/top.png";
+import logo from "../../assets/logo.png";
 
 const screenWidth = Dimensions.get("screen").width;
 
@@ -8,6 +9,23 @@ export default function Basket() {
         <>
             <Image source={top} style={styles.image} />
             <Text style={styles.title}>Detalhe da cesta</Text>
+            <View style={styles.basket}>
+                <Text style={styles.itemTitle}>Cesta de Verduras</Text>
+                <View style={styles.farm}>
+                    <Image style={styles.farmLogo} source={logo} />
+                    <Text style={styles.farmName}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={styles.description}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec fringilla in neque vel efficitur. Nullam leo elit,
+                    tincidunt vitae cursus nec, ornare varius nisl. Proin
+                    vulputate porttitor commodo. Sed sit amet lorem id urna
+                    ullamcorper consequat. Donec iaculis tellus nunc, ut
+                    tincidunt mauris mattis faucibus. Aenean ornare placerat
+                    facilisis.
+                </Text>
+                <Text style={styles.price}>R$ 40,00</Text>
+            </View>
         </>
     );
 }
@@ -18,13 +36,48 @@ const styles = StyleSheet.create({
         height: (578 / 768) * screenWidth,
     },
     title: {
+        fontFamily: "MontserratBold",
         position: "absolute",
         fontSize: 16,
-        fontWeight: "bold",
         textAlign: "center",
         lineHeight: 26,
         padding: 16,
         width: "100%",
         color: "#eee",
+    },
+    basket: {
+        paddingVertical: 16,
+        paddingHorizontal: 20,
+    },
+    itemTitle: {
+        fontFamily: "MontserratBold",
+        color: "#555",
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    farm: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    farmLogo: {
+        width: 32,
+        height: 32,
+        marginRight: 5,
+    },
+    farmName: {
+        color: "#555",
+        fontSize: 14,
+        fontFamily: "Montserrat",
+    },
+    description: {
+        color: "#888",
+        marginBottom: 10,
+        fontFamily: "Montserrat",
+    },
+    price: {
+        color: "#3b6",
+        fontFamily: "MontserratBold",
+        fontSize: 24,
     },
 });
